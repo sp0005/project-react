9c3115ce2f2c6259bd004d2baf06ff752aed12c6
hiragyawali67 <162146274+hiragyawali67@users.noreply.github.com>
2025-05-08 09:41:05 +0545

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Registrationform from './pages/Registrationform';
 import Loginform from './pages/Loginform';
+import ForgotPassword from './pages/Forgotpassword';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Loginform />} />
         <Route path="/register" element={<Registrationform />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
